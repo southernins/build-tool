0009df3082cdcbdaafc08167adc77b18aa6cc7e5
Nathan Crews <ncrews@southerninsurance.net>
2017-11-22 10:36:17 -0600

    testing composer install

--- src/BuildSerivceProvider.php
+++ src/BuildSerivceProvider.php
@@ -7,15 +7,21 @@ use Illuminate\Support\ServiceProvider;
 class BuildServiceProvider extends ServiceProvider {
 
     public function boot(){
+
         if( $this->app->runningInConsole() ){
             $this->commands([
-                Commands\BuildCommand::class,
-                Commands\BuildDevCommand::class
+                Commands\BuildCommand::class
             ]);
         }
+
     } //- END function boot()
 
     public function register(){
+        if( $this->app->runningInConsole() ){
+            $this->commands([
+                Commands\BuildDevCommand::class
+            ]);
+        }
 
     } //- END function register()
 
