37108e66e66974df2e67df143d9b6f6c53ad79e0
Nathan Crews <ncrews@southerninsurance.net>
2020-03-05 16:47:58 -0600

    add tests

---
+++ tests/Unit/shell/GitTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use SouthernIns\BuildTool\Shell\Git;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+
+class GitTest extends TestCase
+{
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testGitBranch()
+    {
+        SELF::setBranchMaster();
+
+        $isBranchMaster = Git::branchName() == 'master';
+
+        $this->assertEquals( true , $isBranchMaster  );
+    }
+
+    static function setBranchMaster(){
+
+        $gitBranch = new Process( "git checkout master" );
+        $gitBranch->setTimeout(180);
+        $gitBranch->run();
+
+        if( !$gitBranch->isSuccessful() ){
+            throw new ProcessFailedException( $gitBranch );
+        }
+
+
+        return $gitBranch->isSuccessful();
+
+    }
+
+
+}
