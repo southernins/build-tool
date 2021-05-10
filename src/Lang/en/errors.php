09217d06f4d89adabcbe3e0300f6a4f0b8057e9e
ncrewsSouthern <ncrews@southerninsurance.net>
2021-05-09 22:53:13 -0500

    move messages to Lang file

---
+++ src/Lang/en/errors.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Build Tool Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following Error messages output from the build tool.
+    |
+    */
+
+    'npm_check' => 'node_modules Folder not found. Run npm install',
+    'composer_check' => 'vendor Folder not found. Run composer install"',
+    'terminated' => 'Build Process Terminated!'
+
+];
